f8bf0226c3c0637fcc69c76a0449bb4a282ec28f
Gustavo Farias <gustavo.faraujo@upe.br>
2026-06-17 21:32:18 -0300

    Implement lowest_common_ancestor function

--- challenges/lowest_common_ancestor.py
+++ challenges/lowest_common_ancestor.py
@@ -6,4 +6,20 @@ def lowest_common_ancestor(
     value1: int,
     value2: int,
 ) -> int:
-    raise NotImplementedError
+    if root is None:
+        return None
+    
+    if root.value == value1 or root.value == value2:
+        return root.value
+    
+    left = lowest_common_ancestor(root.leftm value1, value2)
+
+    right = lowest_common_ancestor(root.right, value1, value2)
+
+    if left is not None and right is not None:
+        return root.value
+    
+    if left is not None:
+        return left
+    
+    return right
